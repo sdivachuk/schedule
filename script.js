b7dc1f3f2be09c7fda08ae9c9128b51a613ffbb8
Sergey Divachuk <sergeydivachuk@gmail.com>
2022-05-31 23:03:56 -0400

    added js for background

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ $("#currentDay").text(moment().format("ddd, MMM, Qo, YYYY"));
 
 var currentTime = moment().hour();
 
+function hourlyPlanner() {
+    $(".time-block").each(function() {
+       var chosenTime = parseInt($(this).attr("id").split("hour")[1]);
+
+       if (currentTime > chosenTime) {
+           $(this).addClass("future")
+       }
+       if (currentTime == chosenTime) {
+           $(this).addClass("present")
+       }
+       if (currentTime < chosenTime) {
+           $(this).addClass("past")
+       }
+    })
+}
